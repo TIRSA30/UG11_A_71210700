a8abee0af362920da7c0b9d3908d2f6c72bd5e34
TIRSA30 <93311704+TIRSA30@users.noreply.github.com>
2021-11-26 01:07:44 +0700

    4_A_71210700

---
+++ 4_A_71210700.py
@@ -0,0 +1,15 @@
+#Nomor 4
+
+def ambildanbalik(kalimat,mulai,akhir,data):
+    if(data):
+        A = kalimat[mulai-1:akhir]
+        return(A[::-1])
+    elif(not data):
+        return(kalimat[mulai-1:akhir])
+    else:
+        print("Maaf, salah input")
+
+print(ambildanbalik("abcdefg",2,4,True))
+print(ambildanbalik("abcdefg",1,5,False))
+print(ambildanbalik("Qwerty",3,4,True))
+print(ambildanbalik("Qwerty",2,2,False))
